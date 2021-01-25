1c3a3110ff7fc95d114ef6b1a2ac5858ea881e71
Oliwia <marsmusic@op.pl>
2021-01-25 23:49:47 +0100

    Add filter for books

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 
-
 const templates = {
   templateBook: Handlebars.compile(document.querySelector('#template-book').innerHTML)
 };
@@ -21,23 +20,70 @@ render();
 initActions();
 
 
-let favoriteBooks = [];
+let favoriteBooks = []
 
 function initActions() {
   const images = document.querySelectorAll('.books-list .book .book__image');
+
 
   for (let image of images) {
     image.addEventListener('dblclick', function (event) {
       event.preventDefault();
       let bookId = image.getAttribute('data-id');
 
-      if(favoriteBooks.includes(bookId)){
+      if (favoriteBooks.includes(bookId)) {
         image.classList.remove('favorite');
         favoriteBooks.splice(bookId);
       } else {
         image.classList.add('favorite');
         favoriteBooks.push(bookId);
-      };    
+
+      };
+
     });
   }
-}
+  let filter = document.querySelector('.filters form')
+  filters = [];
+
+  filter.addEventListener('click', function (event) {
+    if (event.target.name == 'filter' && event.target.type == 'checkbox') {
+    };
+
+    if (event.target.checked == true) {
+      filters.push(event.target.value)
+      hiddenBook();
+
+    } else if (event.target.checked == false) {
+      filters.splice(event.target.value, 1)
+      hiddenBook();
+    };
+  })
+};
+
+function hiddenBook() {
+
+  for (let book of dataSource.books) {
+    let shouldBeHidden = false;
+
+    for (const filter of filters) {
+      if (!book.details[filter]) {
+        shouldBeHidden = true;
+        break;
+      };
+    };
+
+    let dataBook = document.querySelector('.book__image[data-id=' + '"' + book.id + '"]');
+
+    if (shouldBeHidden == true) {
+      dataBook.classList.add('hidden');
+
+    } else if (shouldBeHidden == false) {
+      dataBook.classList.remove('hidden');
+    };
+  };
+};
+
+
+
+
+
